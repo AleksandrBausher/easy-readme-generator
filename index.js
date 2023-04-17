386928e8cb6995576112b0ca911a9eccad160977
AleksandrBausher <bausheraleksandr@gmail.com>
2023-04-17 19:38:55 -0400

    added a function to return readme.md file as a string

--- index.js
+++ index.js
@@ -1,60 +1,92 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
 const questions = [
-    {
-      type: "input",
-      name: "title",
-      message: "Title of your ptoject: ",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Description of your ptoject: ",
-    },
-    {
-      type: "input",
-      name: "istallation",
-      message: "Installation instructions: ",
-    },
-  
-    {
-      type: "input",
-      name: "usage",
-      message: "Usage: ",
-    },
-    {
-      type: "input",
-      name: "contri",
-      message: "Contribution Guidelines: ",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Your GitHub Username: ",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Email Address: ",
-    },
-    {
-      type: "list",
-      message: "License:",
-      name: "license",
-      choices: [
-        "MIT",
-        "Mozilla Public License 2.0",
-        "Open Software License 3.0",
-        "Microsoft Public License",
-      ],
-    },
-  ];
+  {
+    type: "input",
+    name: "title",
+    message: "Title of your ptoject: ",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of your ptoject: ",
+  },
+  {
+    type: "input",
+    name: "istallation",
+    message: "Installation instructions: ",
+  },
 
-async function init(){
-    var userAnswers = await inquirer.prompt(questions)
-    console.log(userAnswers)
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage: ",
+  },
+  {
+    type: "input",
+    name: "contri",
+    message: "Contribution Guidelines: ",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Your GitHub Username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email Address: ",
+  },
+  {
+    type: "list",
+    message: "License:",
+    name: "license",
+    choices: [
+      "MIT",
+      "Mozilla Public License 2.0",
+      "Open Software License 3.0",
+      "Microsoft Public License",
+    ],
+  },
+];
+
+async function init() {
+  var userAnswers = await inquirer.prompt(questions);
+  var readme = createReadme(
+    userAnswers.title,
+    userAnswers.description,
+    userAnswers.installation,
+    userAnswers.usage,
+    userAnswers.contri,
+    userAnswers.email,
+    userAnswers.github,
+    userAnswers.license
+  );
+  console.log(readme);
 }
 
-init()
+function createReadme(
+  title,
+  description,
+  installation,
+  usage,
+  contribution,
+  email,
+  github,
+  license
+) {
+  var readme = `
+${title}
+${description}
+${installation}
+${usage}
+${contribution}
+[${email}](mailto:${email})
+[${github}](https://github.com/${github})  
+${license}
+`;
+  return readme;
+}
+
+init();
